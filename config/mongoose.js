242e99be60b2a527613bea1e8d1b563606067bf1
luck22y <luckykumar2204@gmail.com>
2023-06-11 16:33:17 +0530

    db connected

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+mongoose.connect('mongodb://121.0.0.1:27017/Ecom-API')
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Error connecting server"));
