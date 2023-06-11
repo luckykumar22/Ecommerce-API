ea6daef3044f5bee0515d853f0aa196f2a78c8e0
luck22y <luckykumar2204@gmail.com>
2023-06-11 18:32:01 +0530

    controller added

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require("express");
-const port = 8000;
 const mongoose = require("mongoose");
+const port = 8000;
 
 const app = express();
+const db = require("./config/mongoose");
+const Routes = "./routes/index.js";
+
+// to read form data
+app.use(express.urlencoded);
+
+app.set("view engine", "ejs");
+app.set("views", "./");
+
+//routes
+app.use("/", Routes);
 
 app.listen(port, function (err) {
   if (err) {
     console.error("Error in setting up server");
     return;
-  } else {
-    console.log("Server up and running on port:", port);
   }
+  console.log("Server up and running on port:", port);
 });
